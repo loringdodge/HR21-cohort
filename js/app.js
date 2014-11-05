3aaf9fbfa9c756b6dc5f9d8edfc9398b2f7a644c
Loring Dodge <loringdodge@yahoo.com>
2014-11-04 22:30:40 -0800

    Added new stylings and hover event on person

--- js/app.js
+++ js/app.js
@@ -4,18 +4,12 @@ App.Router.map(function() {
   this.route("profile", { path: "/profile" });
 });
 
+
 App.IndexRoute = Ember.Route.extend({
   model: function() {
-  	var array = [
-      { person : "Loring Dodge", image : "images/carolinewong.jpg" },
-      { person : "Mike Hart", image : "images/alexzai.jpg" },
-      { person : "Jimmy Gong", image : "images/jimmygong.jpg" },
-  	];
-  	for(var i = 0; i < 31; i++){
-  		array.push({ person : "person", image : ""});
-  	}
-    return array;
+  	return People;
   },
+
 });
 
 App.ProfileRoute = Ember.Route.extend({
@@ -24,7 +18,25 @@ App.ProfileRoute = Ember.Route.extend({
       id : "123",
       firstName : "Loring",
       lastName : "Dodge",
-      image : "images/alexzai.jpg"
+      image : "images/alexzai.jpg",
+      bio : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "
     }
   }
 });
+
+App.IndexController = Ember.Controller.extend({
+  templateName: 'index',
+  firstName : "",
+  lastName : "",
+  events: {
+    nameShow: function(firstName, lastName) {
+      this.set('firstName', firstName).set('lastName', lastName);
+    },
+  }
+});
+
+var People = [
+  { firstName : "Loring", lastName : "Dodge", image : "images/carolinewong.jpg" },
+  { firstName : "Mike", lastName : "Hart", image : "images/alexzai.jpg" },
+  { firstName : "Jimmy", lastName : "Gong", image : "images/jimmygong.jpg" },
+];
