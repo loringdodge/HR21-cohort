9fbc4a41eda6615e5682ddd14788fabcb37bf369
Loring Dodge <loringdodge@yahoo.com>
2014-11-19 09:53:59 -0800

    Made a bunch of changes

--- js/app.js
+++ js/app.js
@@ -1,26 +1,36 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
-  this.route("profile", { path: "/profile" });
+  this.route("profile", { path: "/profile/:profile_id" });
 });
 
 
 App.IndexRoute = Ember.Route.extend({
   model: function() {
-  	return People;
+  	return $.getJSON("http://localhost:8888/HR21-cohort/server/response.json").then(function(response) {
+      var array = [];
+      for(var id in response.profile){
+        array.push(response.profile[id])
+      }
+      return array;
+    });
   },
 
 });
 
 App.ProfileRoute = Ember.Route.extend({
-  model: function() {
-    return {
-      id : "123",
-      firstName : "Loring",
-      lastName : "Dodge",
-      image : "images/alexzai.jpg",
-      bio : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "
-    }
+  model: function(params) {
+    return Ember.RSVP.hash({
+          people: $.getJSON("http://localhost:8888/HR21-cohort/server/response.json").then(function(response) {
+                    return response.profile[params.profile_id];
+                  }),
+          github: $.getJSON("https://api.github.com/users/loringdodge").then(function(response) {
+                    var person = [];
+                    person.push(response.company);
+
+                    return person;
+                  }),
+    })
   }
 });
 
@@ -36,7 +46,16 @@ App.IndexController = Ember.Controller.extend({
 });
 
 var People = [
-  { firstName : "Loring", lastName : "Dodge", image : "images/carolinewong.jpg" },
-  { firstName : "Mike", lastName : "Hart", image : "images/alexzai.jpg" },
-  { firstName : "Jimmy", lastName : "Gong", image : "images/jimmygong.jpg" },
+  { id: "123", firstName : "Loring", lastName : "Dodge", image : "images/carolinewong.jp", bio : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. " },
+  { id: "124", firstName : "Mike", lastName : "Hart", image : "images/alexzai.jp", bio : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. " },
+  { id: "125", firstName : "Jimmy", lastName : "Gong", image : "images/jimmygong.jp", bio : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. " },
 ];
+
+var Loring = {
+    "123" : {
+      firstName : "Loring",
+      lastName : "Dodge",
+      image : "images/alexzai.jpg",
+      bio : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "
+    }
+}
